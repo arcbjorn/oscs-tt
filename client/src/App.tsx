e63ab9e3346ea268d5f2f056979e22d071d651db
arcbjorn <arky.happ@gmail.com>
2020-12-12 16:19:39 +0300

    feat (global): boilerplate clean-up & MUI config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <img className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>HELLO NASTYA</p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </Box>
+    </Container>
   );
 }
 
